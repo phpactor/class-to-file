72940c4bc612e3dfdb0e4616d7d09461d65d099c
dantleech <dan.t.leech@gmail.com>
2017-06-26 15:33:39 +0100

    Added namespace + name functions

--- lib/ClassName.php
+++ lib/ClassName.php
@@ -23,6 +23,16 @@ final class ClassName
         return $this->fullyQualifiedName;
     }
 
+    public function namespace()
+    {
+        return substr($this->fullyQualifiedName, 0, strrpos($this->fullyQualifiedName, '\\'));
+    }
+
+    public function name()
+    {
+        return substr($this->fullyQualifiedName, strrpos($this->fullyQualifiedName, '\\') + 1);
+    }
+
     public function beginsWith($prefix)
     {
         return 0 === strpos($this->fullyQualifiedName, $prefix);
